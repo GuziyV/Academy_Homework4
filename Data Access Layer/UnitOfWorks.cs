c18185a2b6da43c477c1838ff97979fb3d7e2952
GuziyV <volodyaguziy3@gmail.com>
2018-07-10 21:37:18 +0300

    Implement UnitOfWork

---
+++ Data Access Layer/UnitOfWorks.cs
@@ -0,0 +1,18 @@
+﻿using Data_Access_Layer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer
+{
+    class UnitOfWorks : IUnitOfWorks
+    {
+
+        public IRepository<T> GetRepository<T>() where T : class
+        {
+            return new Repositories
+        }
+    }
+}
